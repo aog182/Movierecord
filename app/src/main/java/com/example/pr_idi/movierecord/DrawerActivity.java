c1a91020f87a81d4bc1d67d5a51278721b76cf8b
Arnau Orriols Garcia <arnau.orriols.garcia@est.fib.upc.edu>
2017-01-02 00:58:30 +0100

    Afegir pelis done

--- app/src/main/java/com/example/pr_idi/movierecord/DrawerActivity.java
+++ app/src/main/java/com/example/pr_idi/movierecord/DrawerActivity.java
@@ -4,11 +4,13 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -17,21 +19,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-
-import java.util.List;
-import java.util.Random;
-
-import static android.R.attr.fragment;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class DrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private FilmData filmData;
-
+    private String titol_Text = "";
+    private String pais_Text = "";
+    private int any_Text;
+    private String director_Text = "";
+    private String prota_Text = "";
+    private int puntuacio_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +40,14 @@ public class DrawerActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        filmData = new FilmData(getApplicationContext());
+        filmData.open();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                dialogAfegirPeli();
             }
         });
 
@@ -130,5 +132,69 @@ public class DrawerActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void dialogAfegirPeli(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Afegir peli");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setHint("*Títol");
+        layout.addView(input);
+
+        final EditText input2 = new EditText(this);
+        input2.setInputType(InputType.TYPE_CLASS_TEXT);
+        input2.setHint("*País");
+        layout.addView(input2);
+
+        final EditText input3 = new EditText(this);
+        input3.setInputType(InputType.TYPE_CLASS_TEXT);
+        input3.setHint("*Any d'estrena");
+        layout.addView(input3);
+
+        final EditText input4 = new EditText(this);
+        input4.setInputType(InputType.TYPE_CLASS_TEXT);
+        input4.setHint("*Director");
+        layout.addView(input4);
+
+        final EditText input5 = new EditText(this);
+        input5.setInputType(InputType.TYPE_CLASS_TEXT);
+        input5.setHint("*Actor protagonista");
+        layout.addView(input5);
+
+        final EditText input6 = new EditText(this);
+        input6.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input6.setHint("Puntuació de l'1 al 5");
+        layout.addView(input6);
+
+        builder.setView(layout);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                titol_Text = input.getText().toString();
+                pais_Text = input2.getText().toString();
+                any_Text = Integer.parseInt(input3.getText().toString());
+                director_Text = input4.getText().toString();
+                prota_Text = input5.getText().toString();
+                puntuacio_Text = Integer.parseInt(input6.getText().toString());
+
+                filmData.createFilm(titol_Text,director_Text,pais_Text,prota_Text,any_Text,puntuacio_Text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
